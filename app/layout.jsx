04b0ecd2e4a13740ac8917bae3c27331ce56a118
Alex MacLean <maclean199@gmail.com>
2023-09-16 12:33:30 -0400

    Removes extra title

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en' className='antialiased'>
-    <head ><title>AR Soundwalk</title></head>
+    <head />
     <body>{children}</body>
     </html>
   )
